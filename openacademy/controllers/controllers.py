cda4b61bfc0dcaa74bb2f478330c16cf2821859c
Carmen Miranda <carmen@vauxoo.com>
2019-08-30 13:16:30 -0500

    [ADD] openacademy: Adding Website support
    Adding some templates that corresponds to the website support

--- openacademy/controllers/controllers.py
+++ openacademy/controllers/controllers.py
@@ -2,13 +2,20 @@
 from odoo import http
 
 class Openacademy(http.Controller):
-    @http.route('/openacademy/openacademy/', auth='public')
+    @http.route('/openacademy/openacademy/', auth='public', website=True)
     def index(self, **kw):
         Teachers = http.request.env['openacademy.teachers']
         return http.request.render('openacademy.index', {
             'teachers': Teachers.search([]),
         })
 #         return "Hello, world"
+
+    @http.route('/openacademy/<model("openacademy.teachers"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('openacademy.biography', {
+            'person': teacher
+        })
+
 
 #     @http.route('/openacademy/openacademy/objects/', auth='public')
 #     def list(self, **kw):
